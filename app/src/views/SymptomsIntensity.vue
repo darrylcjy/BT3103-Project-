c9529cfd482e23ffdb8edbbeabe33a92199d7d60
darrylcjy <77625209+darrylcjy@users.noreply.github.com>
2022-03-14 06:45:10 +0800

    Added Symptoms Intensity Page

---
+++ app/src/views/SymptomsIntensity.vue
@@ -0,0 +1,17 @@
+<template>
+  <Symptoms3 />
+</template>
+
+<script>
+import Symptoms3 from "@/components/Symptoms3.vue";
+
+export default {
+  name: "Intensity",
+  components: {
+    Symptoms3,
+  },
+};
+</script>
+
+<style>
+</style>
