7632c20b61c7eb4fcc5333dd0f5e18259f940426
MinErn-Chen <77660492+MinErn-Chen@users.noreply.github.com>
2022-03-13 10:00:15 +0800

    First instantiation of medical facilities page

---
+++ app/src/components/MedFacilsDetails.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- get the user name here and replace placeholder {username} and {facilType}-->
+  <div>
+    <div class="header">
+      <h2>
+        <b
+          >{{ username }}, for the symptoms you present, we reccommend you to
+          visit a <u>{{ facilType }}</u
+          >.</b
+        >
+      </h2>
+      <h2>The following are the closest {{ facilType }}s to you:</h2>
+      <br /><br />
+    </div>
+
+    <div
+      class="scrollable"
+    >
+      <!-- add v-for when clinic list obtained -->
+      <div class="card" v-on:click="this.$router.push({path: '/facil-details'})">
+        <h3 id="scrollspyHeading1">{{ clinicName }}</h3>
+        <h4>{{ location }} {{ unitno }} {{ postalCode }}</h4>
+        <h4>Distance: {{ dist }}km away</h4>
+        <h4>Number of patients in queue: {{ queueLen }}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { urlopen } from 'urllib/request';
+// import { BeautifulSoup } from 'bs4';
+
+//import FacilDetails from "@/views/FacilDetails";
+
+export default {
+  data() {
+    return {
+      username: "John Doe",
+      // replace this when clinic data can be obtained
+      facilType: "GP/ Polyclinic",
+      clinicName: "1 BISHAN MEDICAL",
+      location: "283 BISHAN STREET 22",
+      unitno: "#01-191",
+      postalCode: "SINGAPORE 750283",
+      dist: Math.round(Math.random() * 100) / 10,
+      queueLen: Math.floor(Math.random() * 11),
+    };
+  },
+  methods: {
+    getClinicData() {
+      console.log("something");
+      // get the url from the .ipyny file
+    },
+  },
+  /*
+  components:{
+    FacilDetails
+  }
+  */
+};
+</script>
+
+<style scoped>
+.card {
+  text-align: left;
+  padding: 10px;
+  width: 850px;
+  height: 180px;
+  background-color: rgba(183, 218, 250, 1);
+  border-radius: 10px;
+  font-size: 18px;
+}
+.card:hover{
+  background-color: skyblue;
+}
+.scrollable{
+  overflow: scroll; 
+  width: 900px;
+  margin: auto;
+  height: 600px;
+}
+</style>
