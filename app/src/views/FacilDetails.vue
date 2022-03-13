04e0c0bed4fc76dd5a3222f36e1af8b7d3030748
MinErn-Chen <77660492+MinErn-Chen@users.noreply.github.com>
2022-03-13 09:57:19 +0800

    Addded facilty details page

---
+++ app/src/views/FacilDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <FacilDetails/>
+</template>
+
+<script>
+import FacilDetails from '../components/FacilDetails.vue' 
+
+export default {
+    name:'Facility Details',
+        components: {
+            FacilDetails,
+        }
+}
+</script>
+
+<style>
+
+</style>
