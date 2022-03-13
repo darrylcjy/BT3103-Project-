fd05f152cdde438b20628a6e8facc40c52bb35ed
Arjan Singh <arjan9696@gmail.com>
2022-03-13 12:27:28 +0800

    added style for getting started

--- app/src/components/GettingStartedPage.vue
+++ app/src/components/GettingStartedPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="main">
     <div class="btns">
-      <button class="btn" type="button">I'm COVID positive</button>
-      <button class="btn" type="button">I'm a Close Contact</button>
+      <button class="btn1" type="button">I'm COVID positive</button>
+      <button class="btn2" type="button">I'm a Close Contact</button>
     </div>
 
     <div class="img">
@@ -20,18 +20,52 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .main {
-
+    max-width: 750px;
+    margin: auto;
+    padding: 1rem 2rem 1rem 1rem;
   }
 
   .btns {
-
+    max-width: 650px;
   }
 
-  .btn {
-    
+  .btn1 {
+    all:unset;
+    position: absolute;
+    width: 200px;
+    height: 50px;
+    left: 450px;
+    top: 150px;
+    background: #F5F5DD;
+
+    cursor: pointer;
+    font-size:1rem;
+    padding: 5px 0.5rem;
+    border-radius: 10px;
+    text-align: center;
+  }
+
+  .btn2 {
+    all:unset;
+    position: absolute;
+    width: 200px;
+    height: 50px;
+    left: 900px;
+    top: 150px;
+    background: #F5F5DD;
+
+    cursor: pointer;
+    font-size:1rem;
+    padding: 5px 0.5rem;
+    border-radius: 10px;
+    text-align: center;
   }
 
   .swab {
-
+    position: absolute;
+    width: 750px;
+    height: 400px;
+    left: 400px;
+    top: 250px;
   }
 </style>
--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -1,22 +1,69 @@
 <template>
-    <img src="../assets/home.png" alt="">
-    <h1>Latest News</h1><hr>
-    <button @click="updateMe()">Click to update</button>
-    <line-chart class="numbers" width=500px :data = "chartdata"></line-chart>
+  <div class = "main">
+    <div class = "intro">
+      <h2 class="guide">Step-by-step guides</h2>
+      <h2 class="question">Have you tested COVID-positive or been exposed to COVID?</h2>
+      <button class="btn" type="button">Click here</button>
+    </div>
+    <div class="latest">
+      <h1 class="news">Latest News</h1><hr>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  methods: {
-    updateMe: function (){
-      this.chartdata = {"Monday": Math.random()*5, "Tuesday": Math.random()*5, "Wednesday": Math.random()*5, "Thursday": Math.random()*5, "Friday": Math.random()*5}
-    }
-  }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .main {
+    position: absolute; 
+  }
 
+  .intro {
+    background: url("../assets/homefiller.jpg");
+    width: 100vh;
+    height: 200px
+  }
+
+  .guide {
+    top: 120px;
+    color: red;
+  }
+
+  .question {
+
+  }
+
+  .btn {
+    all:unset;
+    position: absolute;
+    width: 140px;
+    height: 30px;
+    left: 280px;
+    top: 120px;
+    background: blue;
+
+    cursor: pointer;
+    font-size:1rem;
+    padding: 5px 0.5rem;
+    text-align: center;
+    color: white
+  }
+
+  .latest {
+
+  }
+
+  .news {
+    text-align: left;
+  }
+
+  .numbers {
+
+  }
 </style>
