e8458627e8cf1fc81fab7e2c221e185f00ed97d6
MinErn-Chen <77660492+MinErn-Chen@users.noreply.github.com>
2022-03-13 09:57:53 +0800

    Added list of medical facilities page

---
+++ app/src/views/MedFacils.vue
@@ -0,0 +1,18 @@
+<template>
+  <MedFacilsDetails/>
+</template>
+
+<script>
+import MedFacilsDetails from '../components/MedFacilsDetails.vue' 
+
+export default {
+    name:'Medical Facilities',
+        components: {
+            MedFacilsDetails,
+        }
+}
+</script>
+
+<style>
+
+</style>
