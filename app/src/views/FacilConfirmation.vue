9ae6cb69e33eadc91fd7b205f862f7d6759e0f4f
MinErn-Chen <77660492+MinErn-Chen@users.noreply.github.com>
2022-03-13 09:56:46 +0800

    Added facility confirmation page

---
+++ app/src/views/FacilConfirmation.vue
@@ -0,0 +1,18 @@
+<template>
+  <FacilConfirmation/>
+</template>
+
+<script>
+import FacilConfirmation from '../components/FacilConfirmation.vue' 
+
+export default {
+    name:'Facility Checkout',
+        components: {
+            FacilConfirmation,
+        }
+}
+</script>
+
+<style>
+
+</style>
