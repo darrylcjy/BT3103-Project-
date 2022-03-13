98c41cf57ffd91781d1692d7a1f7964e489c6b7f
darrylcjy <77625209+darrylcjy@users.noreply.github.com>
2022-03-14 06:45:18 +0800

    Added Symptoms Selection Page

---
+++ app/src/views/SymptomsSelection.vue
@@ -0,0 +1,17 @@
+<template>
+  <Symptoms1 />
+</template>
+
+<script>
+import Symptoms1 from "@/components/Symptoms1.vue";
+
+export default {
+  name: "Selection",
+  components: {
+    Symptoms1,
+  },
+};
+</script>
+
+<style>
+</style>
