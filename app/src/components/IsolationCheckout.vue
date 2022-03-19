2f004eb191b4289722f42acdd4c70f5f1df985bd
Claire Lim <e0559833@u.nus.edu>
2022-03-19 18:56:45 +0800

    Restyled self-isolation checkout

--- app/src/components/IsolationCheckout.vue
+++ app/src/components/IsolationCheckout.vue
@@ -1,74 +1,63 @@
 <template>
     <div id = "heading">
-        <h1>Hello {{name}}, </h1>
-        <h2>the symptoms that you present do not require medical attention.</h2>
+        <h1>The symptoms that you present do not require medical attention.</h1>
     </div>
-    <br>
-    <div id="protocol">
-        <p>Please follow the following steps for the next few days.</p>
-        <table>
-            <tr>
-                <th class="no">Day</th>
-                <th>What should I do?</th>
-            </tr>
+    
+    <!-- PROTOCOL --> 
+    <h2>Please follow the following steps for the next few days.</h2>
 
-            <tr>
-                <td class="no">1</td>
-                <td>
-                    For the next 72 hours,
-                    <li>Do not leave your house.</li>
-                    <li>Stop going to school or for work. Cancel all your social activities.</li>
-                    <li>Self isolate from your household members in a separate room.</li>
-                </td>
-            </tr>
+    <div id="first-days">
+        <h2>Day 1 - Day 3</h2> 
+        <li>Do not leave your house.</li>
+        <li>Stop going to school or office. Cancel all your social activities.</li>
+        <li>Self isolate from your household members in a separate room.</li>
+    </div> <br>
 
-            <tr>
-                <td class="no">2</td>
-                <td></td>
-            </tr>
+    <div id="second-days">
+        <h2>Day 4 - ~</h2>
+        Administer an ART test. <br>
 
-             <tr>
-                <td class="no">3</td>
-                <td></td>
-            </tr>
+        <div class="state">
+            If your ART result is negative, <br>
+            congratulations! <br>
+            You can resume your daily activities.
+        </div>
 
-             <tr>
-                <td class="no">4</td>
-                <td>
-                    After 72 hours, take an ART test. <br>
-                    If your ART result is negative, congratulations!
-                    <li>You can resume your daily activities.</li>
-                    If your ART result is positive, don't worry!
-                    <li>Continue to self isolate till your next ART result is negative.</li>
-                </td>
-            </tr> 
+        <div class="state">
+            If your ART result is positive, <br>
+            don't worry! <br>
+            Continue to self isolate till your next ART result is negative.
+              <!-- include pic of positive and negative -->
+        </div>
+    </div> <br>
 
-            <tr>
-                <td class="no">~</td>
-                <td></td>
-            </tr>
-
-            <!-- based on user's personal info, we can show either 7 days or 14 days -->
-            <!-- 7 days for fully vaccinated + children below 12 -->
-            <!-- 14 days for partially vaccinated/ unvaccinated individuals above 12 -->
-            <tr>
-                <td class="no">{{day}}</td>
-                <td>
-                    Congratulations! You may resume your daily activities.
-                </td>
-            </tr>
-        </table>
+    <div id="third-days">
+        <h2> Day 7 </h2>
+        <!-- based on user's personal info, we can show either 7 days or 14 days -->
+        <!-- 7 days for fully vaccinated + children below 12 -->
+        <!-- 14 days for partially vaccinated/ unvaccinated individuals above 12 -->
+        Congratulations! You may resume your daily activities.  <br>
+        If possible, please minimize your social activities for the next 7 days.
     </div>
-    <br>
-    <div id="symptoms">
-        <p>If you present any of the following symptoms,</p>
-        <ul>
-            <li>Prolonged fever</li>
-            <li>Worsening cough</li>
-            <li>Breathing difficulties</li>
-        </ul>
-        <p>Do not hesitate to go to your nearest GP to seek medical attention.</p>
-        
+    
+    <!-- WARNING --> <br><br>
+    <div class="warning">
+        <h2 style="text-align:center"> Contact 995</h2>
+        If you are experiecing any of the following symptoms:
+        <li>Chest pain</li>
+        <li>Shortness of breadth</li>
+        <li>Sudden weakness on one side</li>
+        <li>Oximeter SpO2 reading below 92%</li> <br>
+    </div>
+
+    <div class="warning">
+        <h2 style="text-align:center">Visit the nearest GP</h2>
+        If you are experiencing any of the following symptoms: 
+        <li>Prolonged fever >= 3 days</li>
+        <li>Persistent diarrhea and vomiting</li>
+        <li>Headaches</li>
+        <li>Sudden/Worsening leg pain and swelling</li>
+        <li>Oximeter SpO2 reading is consistently 93% - 94%</li>
     </div>
 </template>
 
@@ -84,25 +73,46 @@ export default {
 </script>
 
 <style>
-    table {
-        border-collapse: collapse;  /*  to join the borders of the table */
-        table-layout: fixed;
-        /* width: 100%; */
-        margin-left: auto;
-        margin-right: auto;
-    }
-    th, td {
-        border: 1px solid black;
+    #first-days, #second-days, #third-days {
         text-align: justify;
-        overflow: hidden;
+        padding: 10px;
+        width: 900px;
+        height: auto;
+        background-color: rgba(183, 218, 250, 1);
+        border-radius: 10px;
+        font-size: 20px;    /* originally 25*/
+        margin: auto;
     }
-    .no{
+
+    #first-days h2, #second-days h2, #third-days h2 {
         text-align: center;
-        width:100px;
+        background-color: #F5F5DD;
+        margin: auto;
+        border-radius: 10px;
+        margin-bottom: 10px;    /* to add space between header and body */
     }
-    #symptoms {
+
+    #second-days, #third-days {
         text-align: center;
-        list-style-position: inside;
+    }
+
+    .state {
+        border: 1px solid black;
+        display: inline-block;
+        padding: 10px; 
+        margin: 25px;
+        text-align: center;
+        width: 40%;
+    }
+
+    .warning {
+        border: 1px solid black;
+        display: inline-block;
+        padding: 10px;
+        margin: 15px;
+        width: 30%;
+        text-align: left;
+        border-radius: 10px;
     }
 
 </style>
