0aa8425524e5ccf287ca4a722e8f4d13eabf49d2
Claire Lim <e0559833@u.nus.edu>
2022-03-19 15:56:11 +0800

    Added routing in Nav bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,13 +2,13 @@
   <!-- testing purposes -->
   <div>
     <router-link to="/login">Login</router-link> |
-    <router-link to="/personal-details">Personal Details</router-link> |
-    <router-link to="/faq">FAQs</router-link> |
+    <!-- <router-link to="/personal-details">Personal Details</router-link> | -->
+    <!-- <router-link to="/faq">FAQs</router-link> | -->
     <router-link to="/">Home</router-link> |
     <router-link to="/user-home">User Home</router-link> |
     <router-link to="/getting-started">Getting Started</router-link> |
     <router-link to="/health-dec">Health Declaration</router-link> |
-    <router-link to="/contact-us">Contact Us</router-link> |
+    <!-- <router-link to="/contact-us">Contact Us</router-link> | -->
     <router-link to="/med-facils">Medical Facilities</router-link> |
     <router-link to="/self-isolation-checkout">Self-Isolation Checkout</router-link> | 
     <router-link to="/Selection">Symptoms Selection</router-link> |
--- app/src/components/NavigationBar.vue
+++ app/src/components/NavigationBar.vue
@@ -4,10 +4,10 @@
         <div id="covidcare-dropdown">
             <button id="covidcare-button">CovidCare</button>
             <div id="covidcare-content">
-                <a href="#">Home</a>
-                <a href="#">Active Appointments</a> 
-                <a href="#">FAQs</a> 
-                <a href="#">Contacts</a> 
+                <a href="#" v-on:click="this.$router.push({path: '/user-home'})">Home</a>
+                <a href="#" v-on:click="this.$router.push({path: '/facil-confirmation/active-appts'})">Active Appointments</a>
+                <a href="#" v-on:click="this.$router.push({path: '/faq'})">FAQs</a> 
+                <a href="#" v-on:click="this.$router.push({path: '/contact-us'})">Contacts</a> 
             </div>
         </div>
 
@@ -18,13 +18,14 @@
         <div id="profile-dropdown">
             <button id="profile-button"> My Profile </button>
             <div id="profile-content">
-                <a href="#">Edit personal details</a>
+                <a href="#" v-on:click="this.$router.push({path: '/personal-details'})">Edit personal details</a>
                 <a href="#">Logout</a>
             </div>
         </div>
 
         <i id = "profile-icon">
             <img src="../assets/user.png" alt="Icon not found">
+
         </i>
 
     </div>
