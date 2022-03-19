462ee3074e1e7ef68d93e25ecbe1e48573dacb83
shantia2909 <shantiakohjiawen@gmail.com>
2022-03-19 18:45:07 +0800

    slight edit to login page

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -6,12 +6,14 @@
             <div class="main">
                 <p class="heading">COVIDCARE</p>
                 <div class="content">This web application is created to guide you through the current COVID-19 procedures. If you are COVID-Positive, a close contact or want to get relevant COVID information, this application is for you. We hope that COVIDCARE will serve you well.</div>
-                <p>Log in now to get started</p>
-
+                <div class="last">
+                    <p>Log in now to get started OR</p>
+                    <button @click="home()">return to home page</button>
+                </div>
             </div>
         </div>
-    <!-- Login Form -->
-    <LoginFirebaseUI id="ui"/>
+        <!-- Login Form -->
+        <LoginFirebaseUI id="ui"/>
 
     </div>
 
@@ -24,6 +26,11 @@ export default {
     name:'Login',
     components: {
         LoginFirebaseUI
+    },
+    methods: {
+        home() {
+            this.$router.push({name:'Home'})
+        }
     }
 }
 </script>
@@ -42,7 +49,6 @@ export default {
     border-top-right-radius: 1.5rem;
     border-bottom-right-radius: 1.5rem;
     min-height: 100vh;
-    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -65,11 +71,55 @@ export default {
 p {
     font-size: 1.25rem;
     font-weight: bold;
+    margin-bottom: 0rem;
 }
 
 #ui{
     padding: 1.5rem;
     width: 50%;
     align-self: center;
+}
+
+button {
+    all:unset;
+    background: none;
+    cursor: pointer;
+    font-size: 1.25rem;
+    font-weight: bold;
+    color:inherit;
+    padding: 0.5rem 1rem;
+    margin-top: 0.7rem;
+    border: 1px solid black;
+    border-radius: 1rem;
+}
+
+button:hover {
+    box-shadow: 1px 1px 4px grey;
+}
+
+@media(max-width: 750px) {
+    .login {
+        flex-direction: column;
+        min-width: 450px;
+        align-items: center;
+    }
+
+    .description {
+        width: 100%;
+        padding: 1.5rem 0rem;
+        border-radius:1.5rem;
+        min-height: auto;
+        background-color: #B7DAFA ;
+        box-shadow: 1px 1px 3px grey;
+        background: linear-gradient(270deg, rgba(183,218,250,1) 45%, rgba(246,237,255,1) 100%);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #ui{
+        width:100%;
+        align-items: center;
+    }
 }
 </style>
