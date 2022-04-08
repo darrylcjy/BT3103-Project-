0b41f13980b5a27af7e2d07f45014f661dff355c
Arjan Singh <arjan9696@gmail.com>
2022-04-09 00:32:25 +0800

    fix time validation

--- app/src/components/FacilConfirmation.vue
+++ app/src/components/FacilConfirmation.vue
@@ -199,7 +199,7 @@ export default {
 
         if (
           (this.opening &&
-          (parseInt(apptTime.slice(0, 2)) < (clinicOpen + 1) ||
+          (parseInt(apptTime.slice(0, 2)) < (clinicOpen) ||
             parseInt(apptTime.slice(0, 2)) > (clinicClose - 1))) || (!this.opening && apptTime < nowTime && apptDate == today)
         ) {
           window.alert(
