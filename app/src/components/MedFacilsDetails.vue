3bdec88d0dc48556489ba658daa11322ec5be9bf
MinErn-Chen <77660492+MinErn-Chen@users.noreply.github.com>
2022-04-10 14:34:15 +0800

    adapted Shantia's scrollbar

--- app/src/components/MedFacilsDetails.vue
+++ app/src/components/MedFacilsDetails.vue
@@ -249,6 +249,28 @@ export default {
   height: 600px;
   max-height: 600px;
 }
+
+/* Scrollbars apply to overflow */
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+
 button {
   all: unset;
   font-size: 1rem;
